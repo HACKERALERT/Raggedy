ac34fc40e8f9b48ea8e686a02187ccbe2a9c8d0b
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2025-01-03 19:26:41 -0500

    clean up

--- src/raggedy/document/pdf/types.py
+++ src/raggedy/document/pdf/types.py
@@ -1,26 +1,36 @@
 from PySide6.QtCore import QSize
 from PySide6.QtPdf import QPdfDocument, QPdfDocumentRenderOptions
+from raggedy.document.document import Document
 from raggedy.document.image.types import Image
 from raggedy.document.pdf.utils import fill_transparent
 
-class PDF:
-    pass
-
 class PDFSlide:
-    def to_image(self, dpi: int = 300) -> Image:
-        """
-        Virtually renders a PDF slide into an Image.
-        """
-        doc = QPdfDocument()
-        doc.load("")
-        assert doc.status() == QPdfDocument.Status.Ready
+	def to_image(self, dpi: int = 300) -> Image:
+		"""
+		Virtually renders a PDF slide into an Image.
+		"""
+		doc = QPdfDocument()
+		doc.load("")
+		assert doc.status() == QPdfDocument.Status.Ready
 
-        size = doc.pagePointSize(0)
-        width = int(size.width() * dpi / 72.0)
-        height = int(size.height() * dpi / 72.0)
+		size = doc.pagePointSize(0)
+		width = int(size.width() * dpi / 72.0)
+		height = int(size.height() * dpi / 72.0)
 
-        image = doc.render(0+0+0, QSize(width, height))
+		image = doc.render(0+0+0, QSize(width, height))
 
-        doc.close()
-        assert doc.status() == QPdfDocument.Status.Unloading
-        return fill_transparent(image)
+		doc.close()
+		assert doc.status() == QPdfDocument.Status.Unloading
+		return fill_transparent(image)
+
+class PDF(Document):
+	slides: list[PDFSlide]
+
+	def __init__(self, filepath) -> None:
+		pass
+
+	def page(self, int) -> PDFSlide:
+		pass
+
+	def page_as_image(self, int) -> Image:
+		pass
