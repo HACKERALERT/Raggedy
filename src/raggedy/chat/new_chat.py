ed9819619946c10688c233ce03f026b5f054c6e9
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2025-01-09 23:30:40 -0500

    new_chat.py: better docs

--- src/raggedy/chat/new_chat.py
+++ src/raggedy/chat/new_chat.py
@@ -6,21 +6,22 @@ def chat(to: str, model: str, temperature: float = -1, num_ctx: int = -1) -> Cha
 	"""
 	Creates a new chat to provider 'to' and model name 'model'.
 	It is the caller's responsibility to ensure 'model' exists for 'to'.
-	If using local providers, make sure to pull the relevant 'model' first.
+	If using local providers like ollama, make sure to pull the relevant 'model' in advance.
 
 	Args:
 		to: the provider, for example, "ollama" or "openai".
-		model: the model name, for example, "llama3.2" or "gpt-4o-mini".
-		temperature (optional): the model temperature to use (0 for most objective)
-		num_ctx (optional): the context window size as an integer
+		model: the model name, for example, "llama3.2", "llama3.2-vision", or "gpt-4o-mini".
+		temperature (optional): the model temperature to use (use 0 for most consistent and objective).
+		num_ctx (optional, only for ollama): the context window size to use as an integer.
 
 	Returns:
-		Chat: a Chat object in which you can .attach() files and .message().
+		Chat: a Chat object in which you can .attach() files and .message() or .message_stream().
 
 	Raises:
 		ProviderNotFoundException: if the 'to' provider is not found or supported.
 	"""
 	if to == "ollama":
 		return OllamaChat(model, temperature, num_ctx)
+	assert num_ctx == -1
 
 	raise ProviderNotFoundException
